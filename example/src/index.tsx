76af9baf940ed8b7a1d1ab082666cbf374b79482
Andy Richardson <andy.john.richardson@gmail.com>
2019-10-30 20:25:41 +0000

    wip 3

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable */
-import React from "react";
+import React, { useContext } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import { FormProvider, useField } from "./dist";
+import { FormProvider, useField, FormContext } from "./dist";
+import { FormState } from "../../src";
 
 function App() {
   return (
@@ -15,16 +16,26 @@ function App() {
 }
 
 const NameField = () => {
+  const ctx = useContext<FormState>(FormContext as any);
+
+  console.log(ctx);
   // @ts-ignore
   const [firstNameProps, firstNameMeta] = useField({
     name: "firstName",
-    validate: a => (!a ? "This is required!" : undefined)
+    validate: a => (!a ? "This is required!" : undefined),
+    initialValue: "hello"
   });
 
   // @ts-ignore
   const [lastNameProps, lastNameMeta] = useField({
     name: "lastName",
-    validate: a => (!a ? "This is required!" : undefined)
+    validate: l => {
+      const val = ctx.fields.firstName.value;
+      if (val && val.length > 5) {
+        return "Last name is required when first name is more than 5";
+      }
+    },
+    initialTouched: true
   });
 
   console.log(firstNameProps, firstNameMeta);
