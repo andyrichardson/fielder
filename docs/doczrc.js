3b0c41c12cfcd98abce503be156a47bd55273027
Andy Richardson <andy.john.richardson@gmail.com>
2020-02-24 17:21:12 -0800

    Add exclusion for root docs readme

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -1,6 +1,7 @@
 export default {
   name: 'Fielder',
   description: 'Documentation for Fielder',
+  files: ['src/**/*.mdx'],
   menu: ['About', 'Guides', 'Api', 'Examples'],
   repository: 'https://github.com/andyrichardson/fielder',
   themeConfig: {
