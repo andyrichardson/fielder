7b259eec042f7710a5dc4bdbb7103091f957654c
Andy Richardson <andy.john.richardson@gmail.com>
2020-02-24 17:15:41 -0800

    Add branching examples to docs

---
+++ examples/3-branching/src/register-form/Form.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useMemo } from 'react';
+import { useForm, FielderProvider } from 'fielder';
+import { SignUp } from './SignUp';
+import { GettingStarted } from './GettingStarted';
+import { Login } from './Login';
+
+export const Form = () => {
+  const [continued, setContinued] = useState(false);
+  const formState = useForm();
+
+  const formPage = useMemo(() => {
+    if (!continued) {
+      return 'init';
+    }
+
+    if (formState.fields.email.value === 'user@mail.com') {
+      return 'login';
+    }
+
+    return 'signup';
+  }, [formState, continued]);
+
+  return (
+    <FielderProvider value={formState}>
+      {formPage === 'init' && (
+        <GettingStarted onNext={() => setContinued(true)} />
+      )}
+      {formPage === 'login' && <Login />}
+      {formPage === 'signup' && <SignUp />}
+    </FielderProvider>
+  );
+};
