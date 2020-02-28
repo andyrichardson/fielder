3b6a2f41743b8a6d9f261e24c4b7cc1dd83506d2
Andy Richardson <andy.john.richardson@gmail.com>
2020-02-28 12:25:20 -0800

    Remove leftover sitemap code

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -7,11 +7,7 @@ export default {
   themeConfig: {
     initialColorMode: 'dark'
   },
-  siteMetadata: {
-    siteUrl: `http://localhost:9000`
-  },
   plugins: [
-    'gatsby-plugin-sitemap',
     {
       resolve: 'gatsby-plugin-offline',
       options: {
