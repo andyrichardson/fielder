b6258f02eeb08f21b82c59d94eb91e92916c2619
Andy Richardson <andy.john.richardson@gmail.com>
2020-02-28 12:20:30 -0800

    Add sitemap generation on build

--- docs/doczrc.js
+++ docs/doczrc.js
@@ -7,9 +7,13 @@ export default {
   themeConfig: {
     initialColorMode: 'dark'
   },
+  siteMetadata: {
+    siteUrl: `http://localhost:9000`
+  },
   plugins: [
+    'gatsby-plugin-sitemap',
     {
-      resolve: `gatsby-plugin-offline`,
+      resolve: 'gatsby-plugin-offline',
       options: {
         precachePages: ['*']
       }
