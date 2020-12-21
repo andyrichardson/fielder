4d56097386cf20610f18e194e0bc81095fd6a792
Andy Richardson <andy.john.richardson@gmail.com>
2020-12-21 19:13:06 +0000

    Update heading link

--- docs/src/components/HeadingLink.tsx
+++ docs/src/components/HeadingLink.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/display-name */
 import React from 'react';
 import { styled } from '@linaria/react';
-import { scale } from '../scale';
 
 const withLink = (Element: 'h1' | 'h2' | 'h3' | 'h4' | 'h5') => (
   props: any
@@ -20,19 +19,6 @@ export const h4 = withLink('h4');
 export const h5 = withLink('h5');
 
 const AnchorLink = styled.a`
-  position: relative;
   color: initial;
   text-decoration: initial;
-
-  &:hover:before {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: calc(100% + ${scale(-1)});
-    font-size: 0.5em;
-    display: flex;
-    align-items: center;
-    filter: brightness(0);
-    content: '🔗';
-  }
 `;
