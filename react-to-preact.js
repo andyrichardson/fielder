d02934e004751ddf78ca5d5dad1425dbcc31a5d9
Andy Richardson <andy.john.richardson@gmail.com>
2021-01-13 10:28:56 +0000

    Update package.json

--- react-to-preact.js
+++ react-to-preact.js
@@ -1,9 +1,11 @@
 module.exports = ({ types: t }) => {
     const imports = {};
 
+    console.log(t);
     return {
         name: 'test',
         visitor: {
+            // Remap React -> Preact imports
             ImportDeclaration: {
                 enter: function (path, state) {
                     const filename = state.file.opts.sourceFileName;
@@ -62,6 +64,47 @@ module.exports = ({ types: t }) => {
                     }
 
                     path.replaceWithMultiple(replacements);
+                }
+            },
+            // Add onInput to useField type
+            TSPropertySignature: {
+                enter: function(path) {
+                    if (path.node.key.name === "onChange") {
+                        const copy = t.cloneNode(path.node);
+                        copy.key.name = "onInput";
+                        path.insertBefore(copy);
+                    }
+                }
+            },
+            // Add onInput to useField body
+            VariableDeclaration: {
+                enter: function(path) {
+                    const firstDec = path.node.declarations[0];
+
+                    if (!firstDec || firstDec.id.name !== "onChange") {
+                        return;
+                    }
+
+                    const onInput = t.variableDeclaration('const', [t.variableDeclarator(
+                        t.identifier('onInput'),
+                        firstDec.id
+                    )]);
+                    path.insertAfter(onInput)
+                }
+            },
+            // Add onInput to useField memoised deps
+            ArrayExpression: {
+                enter: function(path) {
+                    if (path.node.elements.find((i) => t.isIdentifier(i) && i.name === "onChange")) {
+                        path.node.elements.push(t.identifier('onInput'));
+                    }
+                }
+            },
+            ObjectExpression: {
+                enter: function(path) {
+                    if (path.node.properties.find(i => t.isObjectProperty(i) && i.key.name === "onChange")) {
+                        path.node.properties.push(t.objectProperty(t.identifier('onInput'), t.identifier('onInput')));
+                    }
                 }
             }
         }
